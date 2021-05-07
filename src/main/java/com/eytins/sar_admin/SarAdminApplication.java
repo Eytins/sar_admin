0567a818eaa94ce5a1f2fbdb38f8bccb776b57a6
eytins <eytins@foxmail.com>
2021-05-07 22:06:26 +0800

    Initial commit

---
+++ src/main/java/com/eytins/sar_admin/SarAdminApplication.java
@@ -0,0 +1,13 @@
+package com.eytins.sar_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SarAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eytins/sar_admin/SarAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eytins.sar_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
